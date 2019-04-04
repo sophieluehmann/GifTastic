b96368592dbe7df36f34c76a39111496feb39dee
Sophie OT <SophieOT@Sophies-MacBook-Pro.local>
2019-04-04 04:32:41 -0700

    added buttons and form

---
+++ app.js
@@ -0,0 +1,81 @@
+var snacks = ['cheese', 'pretzels', 'apple', 'chocolate', 'almonds', 'cookies', 'vodka', 'Jennifer Lopez'];
+console.log(snacks);
+
+for (var i=0; i<snacks.length; i++) {
+    var a = $("<button>");
+    a.addClass("snacks");
+    a.attr("snack-name", snacks[i]);
+    a.text(snacks[i]);
+    $("#buttons-view").append(a);
+};
+
+function addButton() {
+    $("#buttons-view").empty();
+    for (var i=0; i<snacks.length; i++) {
+        var a = $("<button>");
+        a.addClass("snacks");
+        a.attr("snack-name", snacks[i]);
+        a.text(snacks[i]);
+        $("#buttons-view").append(a);
+    };
+}
+
+
+function displayGifs() {
+    var gifName = $(this).attr("snack-name");
+    var queryURL = "https://api.giphy.com/v1/gifs/search?api_key=JH6y2mxfZu7D4wIPyQa5dmWh3IfY4pG1&q=" + gifName + "&limit=10&offset=0&rating=PG&lang=en";
+    $.ajax({
+        url: queryURL,
+        method: "GET"
+    }).then(function(response){
+        $("#gifs-view").empty();
+        
+        for (i=0; i<response.data.length; i++) {
+            console.log(response.data[i].url);
+            var gifURL = response.data[i].url;
+
+            var newGif = $('<img src="' + gifURL + '">');
+            console.log(newGif);
+            $("#gifs-view").append('<img src="' + gifURL + '">');
+    
+        }
+        
+    });     
+}
+
+$(document).on("click", ".snacks", displayGifs);
+
+//THIS CODE IS FROM EXERCISE IN CLASS
+/*
+$(".gif").on("click", function() {
+    
+    var state = $(this).attr("data-state");
+    console.log(state);
+
+    if (state === "still") {
+      $(this).attr("data-state", "animate");
+      console.log($(this).attr("data-animate"));
+      $(this).attr("src", $(this).attr("data-animate"));
+    }
+
+    if (state === "animate") {
+      $(this).attr("data-state", "still");
+      console.log($(this).attr("data-still"));
+      $(this).attr("src", $(this).attr("data-still"));
+    }
+*/
+
+
+
+$("#add-topic").on("click", function(event) {
+    event.preventDefault();
+
+    // This line grabs the input from the textbox
+    var snack = $("#topic-input").val().trim();
+
+    // The movie from the textbox is then added to our array
+    snacks.push(snack);
+
+    // Calling renderButtons which handles the processing of our movie array
+    addButton();
+});
